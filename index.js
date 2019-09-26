b4b82672be561c3ba118f739028c5ceedd6052be
Dzhigurda A.A <info@fake-mm.ru>
2019-09-26 15:54:58 +0300

    Insert code

---
+++ index.js
@@ -0,0 +1,64 @@
+const mysql = require('mysql')
+
+/**
+ * The adapter for working with the MySQL database through promises
+ */
+class MySQLAsyncAdapter {
+  constructor (config) {
+    this.config = config
+    this.brockerCallBackError = null
+  }
+
+  /**
+   * The function is intended for the message broker. 2 parameters come to the call back - error and request text.
+   * @example db.setBrokerCallBackError((err, sql) => { redisClient.publish('docService.mysql.error', err.toString() + ' ' + sql) })
+   * @param {Function} brockerCallBackError 
+   */
+  setBrokerCallBackError(brockerCallBackError) {
+    this.brockerCallBackError = brockerCallBackError;
+  }
+
+  async connect () {
+    return new Promise((resolve, reject) => {
+      this.db = mysql.createConnection(this.config)
+      this.db.connect((err) => {
+        if (err) {
+          console.log('[ ] MySQL session error')
+          if (typeof this.brockerCallBackError === "function") this.brockerCallBackError(err, "")
+          reject(new Error(err))
+        } else {
+          console.log(`[*] MySQL session start  ${this.config.host}:${this.config.port}`)
+          this._reConnectEvent(this.config)
+          resolve(this)
+        }
+      })
+    })
+  }
+
+  _reConnectEvent (config) {
+    this.db.on('error', (err) => {
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        setTimeout(() => { this.connect(config) }, 1000)
+      } else {
+        if (typeof this.brockerCallBackError === "function") this.brockerCallBackError(err, "")
+        throw err
+      }
+    })
+  }
+
+  async query (sql, values) {
+    return new Promise((resolve, reject) => {
+      this.db.query(sql, values, function (err, rows) {
+        if (err) {
+          if (typeof this.brockerCallBackError === "function") this.brockerCallBackError(err, sql)
+          reject(new Error(err))
+        } else {
+          resolve(rows)
+        }
+      })
+    })
+  }
+  
+}
+
+module.exports = MySQLAsyncAdapter
